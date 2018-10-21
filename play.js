f52b0940814266ac65c734994d4b2458a92bdfc8
Jurgen Tonneyck <piepongwong@macbook-pro-6.home>
2018-10-21 23:34:55 +0200

    decent first version

--- play.js
+++ play.js
@@ -8,10 +8,10 @@ function ImageSrc(source) {
 
 }
 
-function rover(controls, grid, sprites) {
+function rover(coordinates, controls, grid, sprites) {
 
-    this.x = 0
-    this.y = 0
+    this.x = coordinates[0]
+    this.y = coordinates[1]
     this.facing = "r"
     this.images = {}
     
@@ -20,16 +20,15 @@ function rover(controls, grid, sprites) {
     } 
 
     document.addEventListener("keydown", function(e) {
-        debugger
         switch(e.key.toLowerCase()) {
             case controls.left:
+                this.x -= 1
                 if(this.x < 0) this.x = grid[0]
                 this.facing = "l"
                 break;
             case controls.right:
                 this.x += 1
                 this.x % grid[0]
-                debugger
                 this.facing = "r"
                 break;
             case controls.up:
@@ -56,12 +55,46 @@ function obstacle(coordinates, sprite) {
     this.x = coordinates[0]
     this.y = coordinates[1]
 
-    this.image = ImageSrc(sprite)
+    this.images = {ducky: ImageSrc(sprite)}
 
+    this.currentImage = function() {
+        return this.images.ducky
+    }.bind(this)
 }
 
-var roverA = new rover({left: "a", up: "w", right: "d", down: "s"}, [5,5])
-var obstacleA = new obstacle(0,0, "sfsdf")
+var roverA = new rover(
+    [0,0],
+    {
+        left: "a", 
+        up: "w", 
+        right: "d", 
+        down: "s"
+    }, 
+    [5,5], 
+    {
+        l: "./images/wallL.png",
+        r: "./images/wallR.png",
+        d: "./images/wallD.png",
+        u: "./images/wallU.png"
+    })
+
+    var roverB = new rover(
+        [0,0],
+        {
+            left: "j", 
+            up: "i", 
+            right: "l", 
+            down: "k"
+        }, 
+        [5,5], 
+        {
+            l: "./images/wallL.png",
+            r: "./images/wallR.png",
+            d: "./images/wallD.png",
+            u: "./images/wallU.png"
+        })    
+
+var obstacleA = new obstacle([0,0], "./images/ducky.png")
 
 function game(gameElements, dimensions) {
 
@@ -70,10 +103,9 @@ function game(gameElements, dimensions) {
 
     this.gameImages = []
 
-    for(let i = 0; gameElements.length; i++) {
-
+    for(let i = 0; i <  gameElements.length; i++) {
         for(let key in gameElements[i].images) {
-            this.gameElements.push(gameElements[i].images[key])
+            this.gameImages.push(gameElements[i].images[key])
         } 
 
     }
@@ -82,6 +114,7 @@ function game(gameElements, dimensions) {
 
     this.allLoaded = function(cb) {
         for(var i=0; i< this.gameImages.length; i++){
+
             this.gameImages[i].onload = function(){
                 this.imagesLoaded++;
                 if(this.imagesLoaded ==  this.gameImages.length){
@@ -91,11 +124,28 @@ function game(gameElements, dimensions) {
         }
     }.bind(this)
 
-    this.animate() = function() {
-        for(let j = 0; gameElements.length; j++) {
-            ctx.drawImage(gameElements[j], gameElements.x, gameElements.y)
+    this.animate = function() {
+        ctx.clearRect(0,0,canvas.width, canvas.height)
+        for(let j = 0; j < gameElements.length; j++) {
+            ctx.drawImage(gameElements[j].currentImage(), gameElements[j].x, gameElements[j].y)
+        }
+
+        for(let k = 0; k < gameElements.length; k++) {
+            for(let l = k + 1; l < gameElements.length; l++) {
+                //set up interval border collision should set off boom already
+                if(gameElements[k].x == gameElements[l].x && gameElements[k].y == gameElements[l].y) {
+                    console.log("boem!")
+                }
+            }
         }
         window.requestAnimationFrame(this.animate)
-        
+
     }.bind(this)
 }
+
+var theGame = new game([roverA, roverB, obstacleA], [100, 100])
+
+theGame.allLoaded(function() {
+    //debugger
+    theGame.animate()
+})
